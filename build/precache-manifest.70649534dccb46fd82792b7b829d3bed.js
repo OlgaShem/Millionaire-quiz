c9be73d48d98ff5aab06401fb606c1dacfa7fa25
Olha.Shemena <olgalevcun@gmail.com>
2022-02-06 21:42:53 +0200

    build

---
+++ build/precache-manifest.70649534dccb46fd82792b7b829d3bed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4adeb7b9359765a3bd0596a4414ab1c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb36cd701c89279fa060",
+    "url": "/static/css/main.c320a51d.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb36cd701c89279fa060",
+    "url": "/static/js/main.59ffebb0.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  },
+  {
+    "revision": "f8c75481696354bfdabed0ba328d3476",
+    "url": "/static/media/hand.f8c75481.png"
+  }
+]);
