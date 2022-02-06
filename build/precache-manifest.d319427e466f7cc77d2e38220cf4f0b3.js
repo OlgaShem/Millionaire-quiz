898d7b1764fe50a6014906373832bc06dc128395
Olha.Shemena <olgalevcun@gmail.com>
2022-02-06 22:13:10 +0200

    build new

---
+++ build/precache-manifest.d319427e466f7cc77d2e38220cf4f0b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14adb009f7423452d9c5599dd3f520f",
+    "url": "/Millionaire-quiz/index.html"
+  },
+  {
+    "revision": "2a92530d0af9abc25968",
+    "url": "/Millionaire-quiz/static/css/main.c13fc612.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/Millionaire-quiz/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Millionaire-quiz/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a92530d0af9abc25968",
+    "url": "/Millionaire-quiz/static/js/main.59ffebb0.chunk.js"
+  },
+  {
+    "revision": "a65d272d8942bb5044b7",
+    "url": "/Millionaire-quiz/static/js/runtime-main.cc4b5497.js"
+  },
+  {
+    "revision": "f8c75481696354bfdabed0ba328d3476",
+    "url": "/Millionaire-quiz/static/media/hand.f8c75481.png"
+  }
+]);
